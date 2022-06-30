3edafe1547ec11b7aff13299abd905beff6e04cc
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-06-30 23:16:08 +0700

    Add files via upload

---
+++ directpop.js
@@ -0,0 +1,13 @@
+type='text/javascript'>
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://www.profitablecpmgate.com/vdzv2eekd8?key=3c815dac3283cdaa7b2c3fe94777397c','_blank');
+	});
+//]]>
