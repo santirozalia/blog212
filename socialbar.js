0a626261421e940c3d6f9ab807e90f542a70264c
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-07-02 23:02:07 +0700

    Update socialbar.js

--- socialbar.js
+++ socialbar.js
@@ -1,1 +1,1 @@
-type='text/javascript' src='//pl17338893.profitablecpmgate.com/b5/1f/8f/b51f8f1ec2ce309a3f0c26ad6302b395.js' 
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl17338893.profitablecpmgate.com/b5/1f/8f/b51f8f1ec2ce309a3f0c26ad6302b395.js"></scr' + 'ipt>');
