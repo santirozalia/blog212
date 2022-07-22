4e55f7123c99386e1528f2aa023a5dc39d3d6863
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-07-22 03:24:00 -0500

    Update directshopee.js

--- directshopee.js
+++ directshopee.js
@@ -11,7 +11,6 @@ type='text/javascript'>
 	    window.open('https://shope.ee/2AbtPtaCEA','_blank');
 	 }
 	 {
-	    $(this).removeClass('xepo_ads');
 	window.open('https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4','_blank');
 	
 	});
