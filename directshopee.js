efd85a23b02abfb59f8baca0223780ef62e4cae5
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-07-22 11:42:17 -0500

    Update directshopee.js

--- directshopee.js
+++ directshopee.js
@@ -8,7 +8,8 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	var searchStrings = new Array('https://shope.ee/2AbtPtaCEA','https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4');
+	var searchStrings = new Array('https://shope.ee/2AbtPtaCEA','https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4',
+				      'https://www.profitablecpmgate.com/t91if8jv?key=faaeccf2e25c94a20131e28b6d1f3683');
 
 	var arrayLength = searchStrings.length;
 	for (var i = 0; i < arrayLength; i++) {
