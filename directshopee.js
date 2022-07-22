421d8900d54ff6b17f7c60caea216ec88332f522
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-07-22 11:36:50 -0500

    Update directshopee.js

--- directshopee.js
+++ directshopee.js
@@ -8,14 +8,11 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    
 	
-			var locs = ['https://shope.ee/2AbtPtaCEA','https://www.safestgatetocontent.com/adjqz1ppt?key=1df217958dc54f3ebe550466c8b6ebbf'] 
+	var locs = ['https://shope.ee/2AbtPtaCEA','https://www.safestgatetocontent.com/adjqz1ppt?key=1df217958dc54f3ebe550466c8b6ebbf'] 
 
-			for (let i = 0; i < locs.length; i++) {
-				window.open(locs[i])}
-			 
-	
-	  		 
+	for (let i = 0; i < locs.length; i++) {
+		window.open(locs[i])}
+  		 
 	});
 //]]>
