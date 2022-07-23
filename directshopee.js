326dd497a03c8a16aa97969f26dffc28b34ffc93
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-07-23 09:11:44 -0500

    Update directshopee.js

--- directshopee.js
+++ directshopee.js
@@ -2,16 +2,15 @@ type='text/javascript'>
 //<![CDATA[
 	$(document).ready(function()
 	{
-	  $('body').addClass('xepo_ads');
+	  $('body').addClass('test');
 	});
 
-	$(document).on('click','.xepo_ads',function(e)
+	$(document).on('click','.test',function(e)
 	{
-	    $(this).removeClass('xepo_ads');
-	var locs = ['https://shope.ee/2AbtPtaCEA','https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4','https://www.profitablecpmgate.com/t91if8jv?key=faaeccf2e25c94a20131e28b6d1f3683'] 
-
-			for (let i = 0; i < locs.length; i++) {
-				window.open(locs[i])}
-  		 
+	    $(this).removeClass('test');
+	    window.open('https://shope.ee/2AbtPtaCEA','_blank');
+		window.open('https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4');
+		window.open('https://hot21y.blogspot.com/2021/10/vid-hotts.html'); 
+		 
 	});
 //]]>
