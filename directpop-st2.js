28c9e4cd75099ce747f15a5cb745a145f8ad3487
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-08-01 17:10:25 +0700

    Add files via upload

---
+++ directpop-st2.js
@@ -0,0 +1,13 @@
+type='text/javascript'>
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://shope.ee/2AbtPtaCEA','_blank');
+	});
+//]]>
