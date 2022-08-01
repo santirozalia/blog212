492cb02a3a1f99720b9496637d2afdf848ed6adf
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-08-01 13:27:25 +0700

    Add files via upload

---
+++ histats-mp3.js
@@ -0,0 +1,17 @@
+//<![CDATA[
+
+    //START HISTATS
+    var _Hasync= _Hasync|| [];
+	_Hasync.push(['Histats.start', '1,4683119,4,24,200,50,00011111']);
+	_Hasync.push(['Histats.fasi', '1']);
+	_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+	})();
+	//END HISTATS
+	
+ 
+	
+	   
