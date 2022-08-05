ab499f1acc52d48a02df9cec8ea02ba7364f0a49
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-08-05 14:53:45 +0700

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -1,6 +1,7 @@
-language="javascript">
-<!--
-document.write("<a href=\"https://geulis.xyz">");
-document.write("<b>Read : The Luxury Cars</b>");
-document.write("</a>");
-//-->
+//<![CDATA[
+function jumpTo(The Luxury Cars){
+  var url = location.href;               //Saving URL without hash.
+  location.href = "https://geulis.xyz"+The Luxury Cars;                 //Navigate to the target element.
+  history.replaceState(null,null,url);   //method modifies the current history entry.
+}
+//]]>
