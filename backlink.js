7c8ed347a4faf2795577cd3c629a385ce3f06385
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-08-06 15:09:52 +0700

    Update backlink.js

--- backlink.js
+++ backlink.js
@@ -3,5 +3,5 @@
  
  let text = "The Luxury Cars";
 let result = text.link("https://geulis.xyz/");
-document.getElementById("demo1").innerHTML = result;
+document.getElementById().innerHTML = result;
 //]]>
