5a30f3881521339272576d3a2dd3eb7dcbab7e24
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-08-13 09:54:31 +0700

    Update directshopee.js

--- directshopee.js
+++ directshopee.js
@@ -10,13 +10,13 @@ type='text/javascript'>
 	    $(this).removeClass('xepo_ads');
 	     var popUp = window.open('https://shope.ee/2AbtPtaCEA','_blank'); 
 	     var popUp = window.open('https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4','_blank'); 
-	// setTimeout(() =>  window.open('https://www.profitablecpmgate.com/z5sjvv74n6?key=b33e4f69459883f9b5308dd1a49861b4','_blank'), 3000);
-	  	if (popUp == null || typeof(popUp)=='undefined') {  
-   		 alert('Please disable your pop-up blocker and try again.'); 
-		} 
-		else {  
-		    popUp.focus();
-		}
-		
+	printButtonClicked: function(){              
+	var id = (new Date()).getTime();  
+	var myWindow = window.open(window.location.href + '?printerFriendly=true', id, 
+"toolbar=1,scrollbars=1,location=0,statusbar=0,menubar=1,resizable=1,width=800,
+height=600,left = 240,top = 212");
+	$.post("/ajax/friendlyPrintPage", postData).done(function(htmlContent) {
+		myWindow.document.write(htmlContent);
+		myWindow.focus();
 	});
 //]]>
