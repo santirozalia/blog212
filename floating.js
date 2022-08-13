f1e00e4f3cbf8aabdb6e290d5cbc9cbd7f868d3c
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-08-13 21:22:53 +0700

    Add files via upload

---
+++ floating.js
@@ -0,0 +1,12 @@
+
+	 
+	atOptions = {
+		'key' : 'c4b0107755f6c38fe9c928890473ce4b',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rusticswollenbelonged.com/c4b0107755f6c38fe9c928890473ce4b/invoke.js"></scr' + 'ipt>');
+ 
+
