ef9a8e9e7928b233e7b9e1c561b818c99da047c1
santirozalia <108447198+santirozalia@users.noreply.github.com>
2022-09-28 23:51:24 +0700

    Update arsaek.js

--- arsaek.js
+++ arsaek.js
@@ -4,10 +4,9 @@
     	var cek = document.referrer;
 
     	var ars 	= rChoice([	
-		
-							'https://geulis.xyz/',
-							'https://kasef.xyz/', 
-							'https://kasef.co.uk/', 
+		 
+							'https://bloggzone.me/', 
+							 
 							]);
 
 				function rChoice(arr) {
