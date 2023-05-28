ccaa21db1bdf52d731990f759420b4cb1d8b269c
santirozalia <108447198+santirozalia@users.noreply.github.com>
2023-05-28 22:02:20 +0700

    Update socialbar.js

--- socialbar.js
+++ socialbar.js
@@ -1,1 +1,1 @@
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') +'://pl19444806.highrevenuegate.com/17/4c/b4/174cb438c637c32f2ca0a3950d7cf3ce.js'></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') +'//pl19444806.highrevenuegate.com/17/4c/b4/174cb438c637c32f2ca0a3950d7cf3ce.js'></scr' + 'ipt>');
