223e70ec051e04c6adca7d5c454a741dab92821f
santirozalia <108447198+santirozalia@users.noreply.github.com>
2023-08-20 23:36:47 +0700

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -8,6 +8,6 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://www.highwaycpmrevenue.com/ms44f37fde?key=2b60154a6640e24a49db9d2d589c4bde','_blank');
+	    window.open('https://www.highcpmrevenuegate.com/ms44f37fde?key=2b60154a6640e24a49db9d2d589c4bde','_blank');
 	});
 //]]>
