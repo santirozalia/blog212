7659ab420c5f651348d97a9528af9bc7cfef8696
santirozalia <108447198+santirozalia@users.noreply.github.com>
2023-09-06 08:28:22 +0700

    Update banner728x.js

--- banner728x.js
+++ banner728x.js
@@ -1,8 +1,10 @@
-atOptions = {
-		'key' : 'fddb9ce5997687b04aea9b37d09b9fad',
+ 
+	atOptions = {
+		'key' : 'a21fb4cd6ab8157f6ec7d115e078a605',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rusticswollenbelonged.com/fddb9ce5997687b04aea9b37d09b9fad/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitablecreativeformat.com/a21fb4cd6ab8157f6ec7d115e078a605/invoke.js"></scr' + 'ipt>');
+ 
