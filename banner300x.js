da70c265d1c5c7e9a5946cd48054796ee496609a
santirozalia <108447198+santirozalia@users.noreply.github.com>
2023-10-12 18:24:26 +0700

    Update banner300x.js

--- banner300x.js
+++ banner300x.js
@@ -1,13 +1,13 @@
  
 //<![CDATA[
 atOptions = {
-		'key' : 'c4b0107755f6c38fe9c928890473ce4b',
+		'key' : '5891fb6676c0734c6d972b61908af244',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://rusticswollenbelonged.com/c4b0107755f6c38fe9c928890473ce4b/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5891fb6676c0734c6d972b61908af244/invoke.js"></scr' + 'ipt>');
 //]]>
 
  
