9a6be0b4f76396f99ea22eaca2c15538da6790a6
santirozalia <108447198+santirozalia@users.noreply.github.com>
2023-10-12 18:20:42 +0700

    Update banner728x.js

--- banner728x.js
+++ banner728x.js
@@ -1,4 +1,5 @@
  
+ 
 	atOptions = {
 		'key' : 'a21fb4cd6ab8157f6ec7d115e078a605',
 		'format' : 'iframe',
@@ -6,5 +7,5 @@
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitablecreativeformat.com/a21fb4cd6ab8157f6ec7d115e078a605/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/a21fb4cd6ab8157f6ec7d115e078a605/invoke.js"></scr' + 'ipt>');
  
