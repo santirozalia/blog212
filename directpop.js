655d0ea9eff23236e86b26962373fa701c7fb08b
santirozalia <108447198+santirozalia@users.noreply.github.com>
2024-01-31 10:20:27 +0700

    Update directpop.js

--- directpop.js
+++ directpop.js
@@ -1,13 +1,11 @@
-type='text/javascript'>
-//<![CDATA[
-	$(document).ready(function()
-	{
-	  $('body').addClass('xepo_ads');
-	});
+ 
+document.addEventListener('DOMContentLoaded', function () {
+  const urlsToOpen = [
+   
+    'https://www.toprevenuegate.com/e91xy7b56?key=95d78ef4b8b99066f9049f86aef7097f',
+  ];
 
-	$(document).on('click','.xepo_ads',function(e)
-	{
-	    $(this).removeClass('xepo_ads');
-	    window.open('https://www.toprevenuegate.com/e91xy7b56?key=95d78ef4b8b99066f9049f86aef7097f','_blank');
-	});
-//]]>
+  for (const url of urlsToOpen) {
+    window.open(url);
+  }
+});
