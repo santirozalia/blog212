15b055b573799212ab1da7f5aff158c955550a2e
santirozalia <108447198+santirozalia@users.noreply.github.com>
2024-01-31 09:52:07 +0700

    Create bokehindo21.js

---
+++ bokehindo21.js
@@ -0,0 +1,13 @@
+type='text/javascript'>
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://gozaimasen1.blogspot.com/','_blank');
+	});
+//]]>
