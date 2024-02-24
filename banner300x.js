f130f630ad363a476da937b1ba35da8710c37497
santirozalia <108447198+santirozalia@users.noreply.github.com>
2024-02-24 09:04:01 +0700

    Update banner300x.js

--- banner300x.js
+++ banner300x.js
@@ -7,9 +7,10 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/5891fb6676c0734c6d972b61908af244/invoke.js"></scr' + 'ipt>');
-//]]>
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/5891fb6676c0734c6d972b61908af244/invoke.js"></scr' + 'ipt>');
+ //]]>
 
  
 	
  
+	
