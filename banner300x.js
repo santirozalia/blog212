cfd4609513073e80e6cadbfd139e5d3fa6b93743
santirozalia <108447198+santirozalia@users.noreply.github.com>
2024-02-24 09:21:31 +0700

    Update banner300x.js

--- banner300x.js
+++ banner300x.js
@@ -1,6 +1,7 @@
  type='text/javascript'>
 //<![CDATA[
-atOptions = {
+ 
+	atOptions = {
 		'key' : '5891fb6676c0734c6d972b61908af244',
 		'format' : 'iframe',
 		'height' : 250,
@@ -8,7 +9,7 @@ atOptions = {
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/5891fb6676c0734c6d972b61908af244/invoke.js"></scr' + 'ipt>');
- //]]>
+  //]]>
 
  
 	
